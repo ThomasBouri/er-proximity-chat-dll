7aa6f9e801caee809487dd72de45d0e5a5aa41d1
Thomas <thomasbouri@gmail.com>
2026-07-21 11:35:57 -0400

    new module layout

--- src/er/flags.rs
+++
@@ -1,8 +0,0 @@
-use eldenring::cs::CSEventFlagMan;
-use fromsoftware_shared::FromStatic;
-
-pub unsafe fn get_flag(flag_id: u32) -> Option<bool> {
-    let Ok(efm) = (unsafe {CSEventFlagMan::instance()}) else { return None };
-    Some(efm.virtual_memory_flag.get_flag(flag_id))
-}
-
